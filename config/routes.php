c03a5f7d940598a48a7026749f3c443ca13b8636
Robert Gunze <robertgunze@gmail.com>
2016-05-25 19:41:34 +0000

    Added api versioning

--- config/routes.php
+++ config/routes.php
@@ -42,11 +42,14 @@ use Cake\Routing\Router;
  */
 Router::defaultRouteClass('DashedRoute');
 
-Router::extensions(['json', 'xml']);
-
+Router::prefix('api',function(RouteBuilder $routes){
+    $routes->prefix('v1',function(RouteBuilder $routes){
+        $routes->extensions(['json','xml']);
+        $routes->resources('Clients');
+    });
+});
 Router::scope('/', function (RouteBuilder $routes) {
     
-    $routes->resources('Clients');
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
      * its action called 'display', and we pass a param to select the view file
